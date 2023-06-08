791328976aa1992bdfe8fe6ca16c43eea66109f6
kolibri0 <ddr5xxa@mail.ru>
2023-06-08 12:05:34 +0300

    created screen recolder

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import pyautogui, cv2, numpy, datetime
-from datetime import datetime
+import pyautogui, cv2, numpy
 
-
-# width, height = pyautogui.size()
-size = (1280, 768)
-
+size = pyautogui.size()
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
-
-file_name = input() + '.avi'
-fps = 20
+file_name = input().strip().replace(' ', '_') + '.avi'
+fps = 30
 
 video = cv2.VideoWriter(file_name, fourcc, fps, (size))
 
 cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Live", 480, 270)
-
-def pause():
-   cv2.waitKey(0)
 
 while True:
     img = pyautogui.screenshot()
@@ -24,20 +16,15 @@ while True:
     img_final = cv2.cvtColor(numpy_array, cv2.COLOR_BGR2RGB)
 
     video.write(img_final)
-    #  cv2.imshow('recold', img_final)
     cv2.imshow('Live', img_final)
 
     if cv2.waitKey(1) == 32:
-      pause()
+       cv2.waitKey(0)
     elif cv2.waitKey(1) == ord('q'):
       break
 
-
 cv2.destroyAllWindows()
 video.release()
-
-
-
 
 
 
